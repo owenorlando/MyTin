ffd97b6baa97dfc50ee102ef3ea85f920e80eff4
Owen Orlando <oorlando@student.ciputra.ac.id>
2024-05-17 10:25:28 +0700

    Initial Commit

---
+++ MyTin/MyTinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTinApp.swift
+//  MyTin
+//
+//  Created by MacBook Pro on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
